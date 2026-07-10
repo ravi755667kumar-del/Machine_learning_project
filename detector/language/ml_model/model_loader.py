e83cd669a422d8d07f8a91afb8665c514eaa117a
Ravi kumar <ravi755667kumar@gmail.com>
2026-07-10 15:05:47 +0530

    upated the case_sensitive in model.loader

--- detector/language/ml_model/model_loader.py
+++ detector/language/ml_model/model_loader.py
@@ -4,4 +4,4 @@ import joblib
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 model =joblib.load(os.path.join(BASE_DIR, 'model.pkl'))
-vectorizer=joblib.load(os.path.join(BASE_DIR, "Vectorizer.pkl"))
+vectorizer=joblib.load(os.path.join(BASE_DIR, "vectorizer.pkl"))
